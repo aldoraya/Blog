a3ab7dc951afe497f07f108db09b1354de0fbd77
aldoraya <aldorayaalhakim@gmail.com>
2023-06-21 12:16:13 +0700

    Update Folder & File

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -24,9 +24,9 @@ const Blog = async () => {
 
   return (
     <div className="w-full h-max pt-24 md:pt-16">
-      {data.map((item) => (
-        <div key={item.id} className="pb-10 px-8 md:pt-10 md:px-10 lg:px-44">
-          <div className="block">
+      <div className="pb-10 px-8 md:pt-10 md:px-10 lg:px-44">
+        <div className="block">
+          {data.map((item) => (
             <Link href={`/blog/${item._id}`} key={item.id}>
               <div className="block md:flex">
                 <div className="flex justify-start">
@@ -47,9 +47,9 @@ const Blog = async () => {
                 </div>
               </div>
             </Link>
-          </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
